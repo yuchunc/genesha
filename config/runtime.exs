8b20583854a7c5ecd8ac6e1544f8485e2e8a6c7c
Mickey Chen <mickey@remote.com>
2026-09-18 13:12:21 +0800

    Change dev server port to 4006

--- config/runtime.exs
+++ config/runtime.exs
@@ -21,7 +21,7 @@ if System.get_env("PHX_SERVER") do
 end
 
 config :ganesha, GaneshaWeb.Endpoint,
-  http: [port: String.to_integer(System.get_env("PORT", "4000"))]
+  http: [port: String.to_integer(System.get_env("PORT", "4006"))]
 
 if config_env() == :dev do
   # Reload browser tabs when matching files change.
